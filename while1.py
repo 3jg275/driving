f8ac460a1e9594905e40679767090321e31e2b17
3jg275 <3jg275@gmail.com>
2019-04-16 22:58:36 +0900

    first try

---
+++ while1.py
@@ -0,0 +1,11 @@
+while True:
+    mode = input('plz input game mode ')
+    if mode == 'q': #quit
+        print('stop')
+        break
+    elif mode == '1':
+        print('mode 1')
+    elif mode == '2':
+        print('mode 2')
+    else:
+        print('1/2/q only')
