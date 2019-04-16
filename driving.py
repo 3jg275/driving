889e05d76c3b9401141a74c32710e051eaf53e2a
3jg275 <3jg275@gmail.com>
2019-04-16 22:34:02 +0900

    add US

--- driving.py
+++ driving.py
@@ -1,8 +1,14 @@
 country = input('Nation plz ')
+country = str(country)
 age = input('plz input your age ')
 age = int(age)
 if country == 'Taiwan':
     if age >= 18:
+        print('Able to get license')
+    else:
+        print('Not yet')
+elif country == 'us':
+    if age >= 16:
         print('Able to get license')
     else:
         print('Not yet')
